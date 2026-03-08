27d819841e4c156535ea607d777579b2e4022078
HamzaDamouh <damouhamza11@gmail.com>
2026-03-08 17:36:24 +0000

    feat: add formatCurrency utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount: number): string {
+  // Format as 12 450,00 MAD
+  return new Intl.NumberFormat('fr-FR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(amount).replace(/\u202f/g, ' ') + ' MAD'
+}
